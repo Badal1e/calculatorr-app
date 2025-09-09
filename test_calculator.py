31fddfe7b8ecc71793b3e4fe64d68453f2d8a2f8
Your Name <you@example.com>
2025-09-09 16:26:07 +0530

    Comment added

--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,8 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(subtract(5,2),3)
     def test_add(self):
         self.assertEqual(multiply(3,6),18)
+        
 
 if __name__=='__main__':
     unittest.main()
+#new comment added
